93e91a48c871213b1cad6ad781fb06160708022f
Zii <sjm891114@gmail.com>
2021-03-22 00:46:33 +0900

    basic setting

---
+++ nServer/src/user/user.entity.ts
@@ -0,0 +1,22 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  nickname: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  createdAt: Date;
+
+  @Column()
+  updatedAt: Date;
+}
